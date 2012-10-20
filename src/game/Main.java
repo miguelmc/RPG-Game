b6d1c9d59e9b386b2249abf41865102751e6bfe6
miguelmc <miguelmtz93@gmail.com>
2012-10-20 13:47:43 -0500

    .

--- src/game/Main.java
+++ src/game/Main.java
@@ -37,6 +37,7 @@ public class Main
 
 	static //static "constructor"
 	{
+		System.out.println("Hola");
 		XMLParser parser = new XMLParser("game_config.xml");
 
 		NAME = parser.getAttribute("GAME", "name");
