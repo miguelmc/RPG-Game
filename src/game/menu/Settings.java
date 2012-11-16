b8b55101e1defa1808c97e683fb7a8c2fd3201c6
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2012-11-15 22:43:26 -0600

    main menu test

---
+++ src/game/menu/Settings.java
@@ -0,0 +1,32 @@
+package game.menu;
+
+import game.util.MouseManager;
+
+import org.lwjgl.input.Keyboard;
+
+public class Settings {
+
+	public static void input()
+	{
+		while(Keyboard.next())
+		{
+			
+		}
+		
+		while(MouseManager.hasEvent())
+		{
+			
+		}
+	}
+	
+	public static void update()
+	{
+		
+	}
+	
+	public static void render()
+	{
+		
+	}
+	
+}
