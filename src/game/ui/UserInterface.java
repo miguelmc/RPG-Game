3894f6a01e1d88c6d2a849ebce689e1e31fb8dc2
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2012-11-21 02:10:40 -0600

    resizchanged shortcut img position

--- src/game/ui/UserInterface.java
+++ src/game/ui/UserInterface.java
@@ -27,8 +27,8 @@ public class UserInterface
 	private static final Point NOTIFICATIONS_POS = new Point(10, 70);
 	private static Queue<Notification> notifications = new LinkedList<Notification>();
 	private static Queue<Notification> importantNotifications = new LinkedList<Notification>();
-	private static final Dimension BAR_SIZE = new Dimension(128, 25), LEVEL_UP_SIZE = new Dimension(440, 110), SHORTCUTS_SIZE;
-	private static final Point HP_POS = new Point(10, 10), MP_POS = new Point(200, 10), EXP_POS = new Point(390, 10),
+	private static final Dimension BAR_SIZE = new Dimension(148, 25), LEVEL_UP_SIZE = new Dimension(440, 110), SHORTCUTS_SIZE;
+	private static final Point HP_POS = new Point(10, 10), MP_POS = new Point(250, 10), EXP_POS = new Point(490, 10),
 							   MAP_NAME_POS = new Point(10, 40), LEVEL_POS = new Point(Main.DIM.getWidth()-10, 5),
 							   LEVEL_UP_POS, SHORTCUTS_POS;
 	private static final Texture levelUpTexture, skillShortcuts;
@@ -41,9 +41,11 @@ public class UserInterface
 		levelUpTexture = Util.getTexture("UI/levelUp.png");
 		LEVEL_UP_POS = new Point(Main.DIM.getWidth()/2, (int)(Main.DIM.getHeight()*.3));
 		
+		SHORTCUTS_SIZE = new Dimension(384, 84);
+		SHORTCUTS_POS = new Point(Main.DIM.getWidth()/2 - SHORTCUTS_SIZE.getWidth()/2, Main.DIM.getHeight() - SHORTCUTS_SIZE.getHeight());
+	
+	
 		skillShortcuts = Util.getTexture("UI/skillShortcuts.png");
-		SHORTCUTS_SIZE = new Dimension(84, 384);
-		SHORTCUTS_POS = new Point(0, 170);
 	}
 	
 	public static void render()
@@ -58,7 +60,7 @@ public class UserInterface
 			
 		glColor4f(1, 1, 1, .5f); //border
 
-		Renderer.render(new Builder(skillShortcuts, SHORTCUTS_POS, SHORTCUTS_SIZE).imageSize(56, 256));
+		Renderer.render(new Builder(skillShortcuts, SHORTCUTS_POS, SHORTCUTS_SIZE).imageSize(256, 56));
 		glColor4f(1, 1, 1, 1); //border
 
 		Player player = MapManager.getMap().getPlayer();
